c3ee602010ecbed02dc1dd8307d2a3d4b9023e3d
tadeuandrade <tadeuandrade98@hotmail.com>
2019-05-24 22:21:48 -0300

    Update web.php

--- rotas/routes/web.php
+++ rotas/routes/web.php
@@ -9,8 +9,28 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/ola', function () {
+    return "<h1>Seja bem vindo</h1>";
+});
+Route::get('/ola/sejabemvindo', function () {
+    return view('welcome');
+});
+Route::get('/nome/{nome}/{sobrenome}', function($nome, $sn){
+    return "<h1>Ola, $nome $sn</h1>";
+});
+Route::get('/repetir/{nome}/{n}', function($nome, $n){
+    if (is_integer($n)) {
+        for ($i=0; $i <$n ; $i++) { 
+            echo "<h1>Ola, $nome</h1>";
+        }
+    } else {
+        echo "Você não digitou um inteiro";
+    }
+        
 });
