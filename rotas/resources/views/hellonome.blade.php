9c8662d9b0a6fdbdf2c5e2e55a267d97d9809d4d
tadeuandrade <tadeuandrade98@hotmail.com>
2019-05-29 20:41:44 -0300

    aula 1,0

---
+++ rotas/resources/views/hellonome.blade.php
@@ -0,0 +1,1 @@
+<h1>hello, {{$nome}} {{$sobrenome}}</h1>
--- rotas/routes/web.php
+++ rotas/routes/web.php
@@ -31,6 +31,49 @@ Route::get('/repetir/{nome}/{n}', function($nome, $n){
         }
     } else {
         echo "Você não digitou um inteiro";
-    }
-        
+    }        
 });
+Route::get('/seunomecomregra/{nome}/{n}', function($nome, $n){
+    for ($i=0; $i <$n ; $i++) { 
+        echo "<h1>Ola, $nome</h1>";
+    }
+})->where('n','[0-9]+')->where('nome','[A-Za-z]+');
+
+Route::get('/seunomesemregra/{nome?}', function($nome=null){   
+    if(isset($nome)){
+        echo "<h1>Ola, $nome</h1>";
+    }else{
+        echo "voce não passou nome";
+    }
+           
+});
+
+Route::prefix('app')->group(function(){
+    Route::get('/',function(){
+       return "Pagina principal do app"; 
+    });
+    Route::get('profile',function(){
+        return "Pagina profile";        
+    });
+    Route::get('about',function(){
+        return "Meu about";        
+    });
+});
+
+Route::redirect('/aqui', '/ola', 301);
+
+Route::view('/hello', 'hello');
+
+Route::view('/viewnome', 'hellonome', 
+    ['nome'=>'tadeu', 'sobrenome'=>'Silva']
+);
+
+
+Route::get('/hellonome/{nome}/{sobrenome}', function($nome, $sobrenome){
+    return view('hellonome',
+    ['nome'=>$nome, 'sobrenome'=>$sobrenome] );
+});
+
+
+
+
